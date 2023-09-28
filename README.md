e20ac9ffbaf9711fbc5fd62780c6f4a0c04f3039
Habib Rehman <haburrehman@eand.com>
2023-09-28 16:08:16 +0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SmilesSharedModels
+
+A description of this package.
---
+++ Sources/SmilesSharedModels/SmilesSharedModels.swift
@@ -0,0 +1,6 @@
+public struct SmilesSharedModels {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
