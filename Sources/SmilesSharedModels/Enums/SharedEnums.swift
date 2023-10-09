5eec9f7b5ccca8ba1f88265a22bbe47e023af5bf
Habib Rehman <haburrehman@eand.com>
2023-10-09 16:54:18 +0500

    -Changed access level to public

--- Sources/SmilesSharedModels/Enums/SharedEnums.swift
+++ Sources/SmilesSharedModels/Enums/SharedEnums.swift
@@ -8,10 +8,10 @@
 import Foundation
 import UIKit
 
- enum RestaurantMenuType: String, Codable {
+ public enum RestaurantMenuType: String, Codable {
     case DELIVERY, PICK_UP
 }
 
- enum OrderStatus : Int {
+public enum OrderStatus : Int {
     case orderReceived = 0, orderSubmitted = 1, restaurantAccepted = 2, preparingFood = 3,  foodReady = 4, orderPickedUp = 5, outForDelivery = 6, delivered = 7,  orderRejected = 8, changedToPickup = 9, pendingDeliveryConfirmation = 10, orderedCancel = 11, processingYourOrder = 12
 }
--- Sources/SmilesSharedModels/FoodOrder/Food/ReOrderResponseModel.swift
+++ Sources/SmilesSharedModels/FoodOrder/Food/ReOrderResponseModel.swift
@@ -6,7 +6,7 @@
 //
 
 import Foundation
-import NetworkingLayer
+//import NetworkingLayer
 
 class ReOrderResponseModel : BaseMainResponse {
     let reOrderStatus : Bool?
